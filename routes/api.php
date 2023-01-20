1cf1adf62a494beab77265f1a265f80339ba0e1e
snerva <selenenerva@live.it>
2023-01-20 18:50:55 +0100

    fixed image bug

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/projects', [ApiProjectController::class, 'index']);
-Route::get('/projects/{slug}', [ApiProjectController::class, 'show']);
+Route::get('/projects/{project:slug}', [ApiProjectController::class, 'show']);
